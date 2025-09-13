4e63a5d8021f070026c9ff67e91b905cf0abf7bf
hungnq10 <nguyenhungpes18@gmail.com>
2025-09-13 11:46:08 +0700

    fix frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import JobDetail from "./pages/JobDetail";
+import JobDetail from "./pages/jobDetail";
 import JobList from "./pages/JobList";
 
 function App() {
